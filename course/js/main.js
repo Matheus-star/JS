ef7b36bdee0e1164efc80e16e2ca8f7665421979
Matt <matheus2000846@gmail.com>
2020-01-20 20:44:28 -0300

    Atualização 20/01/2020

--- course/js/main.js
+++ course/js/main.js
@@ -15,43 +15,45 @@ function getTotal(list) {
 function setList(list) {
     let table = '<thead><tr><td>Description</td><td>Amound</td><td>Value</td><td>Action</td></tr></thead><tbody><tr>';
     for (const key in list) {
-      table +=   '<tr><td>'+formatDesc(list[key].desc)+'</td><td>'+formatAmount(list[key].amount) +'</td><td>'+formatNumber(list[key].value)+'</td><td><button class="btn btn-default" onclick="('+key+');">Edit</button> | <button class="btn btn-default" onclick="deleteForm('+key+');">Delete</td></tr>';
+      table +=   '<tr><td>'+formatDesc(list[key].desc)+'</td><td>'+list[key].amount +'</td><td>'+formatValue(list[key].value)+'</td><td><button class="btn btn-default" onclick="setUpdate('+key+');">Edit</button> | <button class="btn btn-default" onclick="deleteForm('+key+');">Delete</td></tr>';
     }
     table += '</tbody>';
     document.getElementById("listTable").innerHTML = table;
-
 }
-function formatAmount(value) {
-    return parseInt(amount);
-}
+//Formantando dados
+//function formatAmount(value) {
+  //  return parseInt(amount);
+//}
 function formatDesc(desc) {
     let str = desc.toLowerCase();
     str = str.charAt(0).toUpperCase() + str.slice(1);
     return str;
 }
-function formatNumber(value) {
+function formatValue(value) {
     let num = parseFloat(value).toFixed(2)+ '';
     num = num.replace('.' , ',');
     num = '$ ' + num;
     return num;
 }
-function addData() {
-    let desc = document.getElementById('desc').value;
-    let amount = document.getElementById('amount').value;
-    let value = document.getElementById('value').value;
+function addData() {//Adicionando dados
+    let desc = document.getElementById("desc").value;
+    let amount = document.getElementById("amount").value;
+    let value = document.getElementById("value").value;
     list.unshift({"desc": desc, "amount":amount, "value": value});
     setList(list);
 }
-function setUpdate(id) {
-    let obj = list[id];
+function setUpdate(id) { //Editando Dados 
+    var obj = list[id];
     document.getElementById("desc").value = obj.desc;
     document.getElementById("amount").value = obj.amount;
     document.getElementById("value").value = obj.value;
-    document.getElementById("btnUpdate").style.display = 'inline-block';
-    document.getElementById("btnAdd").style.display = 'none';
-    document.getElementById('inputIDUpdate').innerHTML =  '<input type="hidden" id="idUpdate"></input>';
+    document.getElementById("btnUpdate").style.display = "inline-block";
+    document.getElementById("btnAdd").style.display = "none";
+
+    document.getElementById("inputIDUpdate").innerHTML = '<input id="idUpdate" type="hidden" value="'+id+'">';
 }
-function resetForm() {
+
+function resetForm() { //Deletando Dados
     document.getElementById("desc").value = "";
     document.getElementById("amount").value = "";
     document.getElementById("value").value = "";
@@ -59,40 +61,51 @@ function resetForm() {
     document.getElementById("btnAdd").style.display = 'inline-block';
     document.getElementById('inputIDUpdate').innerHTML =  "";
 }
-function updateData() {
-    let id = document.getElementById('updateData');
+function updateData() { // Salvar alteração de dados
+    let id = document.getElementById('idUpdate').value;
+
     let desc = document.getElementById("desc").value;
     let amount = document.getElementById("amount").value;
     let value =  document.getElementById("value").value;
-    list[id] = {'desc': desc, 'amount':amount , 'value':value }
+    list[id] = {"desc": desc, "amount":amount , "value":value };
     resetForm()
     setList(list);
 }
-function deleteForm(id){
-    if (confirm("Delete this item?")) {
-        if (id === list.length - 1) {
-            list.pop();
-        }else if (id === 0){
-            list.shift();
-        }else{
-            let arrAux = list.slice(0,id);
-            let arrAuxEnd = list.slice(id + 1);
-            list = arrAux.concat(arrAuxEnd);
-        }
-        setList(list);
-    }
-}
-function validation() {
-    let desc = document.getElementById("desc").value;
-    let amount = document.getElementById("amount").value;
-    let value = document.getElementById("value").value;
-    var errors = "";
-    if (desc === "") {
-        errors += '<p>Fill out description</p>';
-    }
-    if (amount === '') {
 
-    }
-}
+
+
+
+
+
+
+
+
+
+//function deleteForm(id){
+   // if (confirm("Delete this item?")) {
+     //   if (id === list.length - 1) {
+       //     list.pop();
+        //}else if (id === 0){
+          //  list.shift();
+        //}else{
+          //  let arrAux = list.slice(0,id);
+            //let arrAuxEnd = list.slice(id + 1);
+            //list = arrAux.concat(arrAuxEnd);
+        //}
+//        setList(list);
+   // }
+//}
+//function validation() {
+  //  let desc = document.getElementById("desc").value;
+   // let amount = document.getElementById("amount").value;
+//    let value = document.getElementById("value").value;
+  //  var errors = "";
+    //if (desc === "") {
+      //  errors += '<p>Fill out description</p>';
+    //}
+    //if (amount === '') {
+
+    //}
+//}
 setList(list);
 console.log(getTotal(list,))
